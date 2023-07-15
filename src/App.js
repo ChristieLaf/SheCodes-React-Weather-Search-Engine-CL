8afc6a3174298178d25c5c1b83d5af74c38c954d
ChristieLaf <133935258+ChristieLaf@users.noreply.github.com>
2023-07-15 19:28:12 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/ChristieLaf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Christie Laferriere{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/ChristieLaf/SheCodes-React-Weather-Search-Engine-CL.git"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
