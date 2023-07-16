b6b7f70a52ac582feb2ded6a8155a2874d29bbb6
ChristieLaf <133935258+ChristieLaf@users.noreply.github.com>
2023-07-15 20:03:20 -0400

    Added bootsrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/ChristieLaf"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          Christie Laferriere{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/ChristieLaf/SheCodes-React-Weather-Search-Engine-CL.git"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/ChristieLaf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Christie Laferriere{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/ChristieLaf/SheCodes-React-Weather-Search-Engine-CL.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
